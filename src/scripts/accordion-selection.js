5d6bded5ef550f17710b1d9e0762135131681c21
Hristo Hristov <hristo_lv96@abv.bg>
2018-06-13 14:38:24 +0300

    Host Search Location + Host type + refactoring
    Adds Location and Host type checkboxes for the host search feature. Moves the script responsible for square symbol choice to a separate file. Moves some CSS to general- search

---
+++ src/scripts/accordion-selection.js
@@ -0,0 +1,46 @@
+$(function() {
+				// Single check
+				$('.accordion .card-body').click(function (e) {
+					var checkbox = $(this).siblings('input[type="checkbox"]');
+					if (checkbox.prop('checked')) {
+						checkbox.prop('checked', false);
+					}
+					else {
+						checkbox.prop('checked', true);
+					}
+				});
+				
+				// Country check
+				$('.accordion .country-checkbox').click(function () {
+					var destination = $(this).siblings('a.open-nav').attr('data-target')
+					var checkbox = $(destination).find('input[type="checkbox"]');
+					
+					if ($(this).prop('checked')) {
+						checkbox.prop('checked', true);
+					}
+					else {
+						checkbox.prop('checked', false);
+					}
+				});
+				
+				// Check state
+				$('.accordion .select-checkbox').click(function() {
+					var selector = $(this).parent();
+					var selectedCheckboxes = selector.find('input[type=checkbox]:checked');
+					
+					var allSelector = selector.attr('aria-labelledby');					
+					var selectAllCheckbox = $('#' + allSelector).find('input[type="checkbox"]')
+
+					if (selectedCheckboxes.length === 0) {
+						selectAllCheckbox.prop("indeterminate", false);
+						selectAllCheckbox.prop('checked', false);
+					} 
+					else if (selector.find('input[type=checkbox]:not(:checked)').length === 0) {
+						selectAllCheckbox.prop("indeterminate", false);
+						selectAllCheckbox.prop('checked', true);
+					} 
+					else {
+						selectAllCheckbox.prop("indeterminate", true);
+					}
+				});
+			});
